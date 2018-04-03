acab8da104247e9cd6c0d6676dc6e6c82ae9030a
brandly <brandly13@gmail.com>
2018-04-03 15:08:17 -0400

    some types

--- REVC/main.hs
+++ REVC/main.hs
@@ -1,11 +1,13 @@
 module Main where
 
+input :: String
 input = "AAAACCCGGT"
 
 main :: IO ()
 main =
   putStrLn $ map complement $ reverse input
 
+complement :: Char -> Char
 complement 'A' = 'T'
 complement 'T' = 'A'
 complement 'G' = 'C'
