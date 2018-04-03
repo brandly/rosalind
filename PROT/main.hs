7cd4235930febabf603df287dfb9b05a1c9c4725
brandly <brandly13@gmail.com>
2018-04-03 19:52:17 -0400

    PROT

---
+++ PROT/main.hs
@@ -0,0 +1,40 @@
+module Main where
+
+import Data.List (tails, intercalate)
+import Data.Monoid ((<>))
+
+import qualified Data.Map as M (Map, fromList, lookup)
+
+input = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+
+main :: IO ()
+main =
+  putStrLn
+    $ intercalate ""
+    $ filter (/="Stop")
+    $ map codonToAmino
+    $ splitLen 3 input
+
+codonToAmino :: String -> String
+codonToAmino codon =
+  case M.lookup codon rnaCodonTable of
+    Just val -> val
+    Nothing -> ""
+
+rnaCodonTable :: M.Map String String
+rnaCodonTable =
+  M.fromList
+    $ listOfTuples
+    -- newlines removed by hand
+    $ words "UUU F      CUU L      AUU I      GUU V   UUC F      CUC L      AUC I      GUC V   UUA L      CUA L      AUA I      GUA V   UUG L      CUG L      AUG M      GUG V   UCU S      CCU P      ACU T      GCU A   UCC S      CCC P      ACC T      GCC A   UCA S      CCA P      ACA T      GCA A   UCG S      CCG P      ACG T      GCG A   UAU Y      CAU H      AAU N      GAU D   UAC Y      CAC H      AAC N      GAC D   UAA Stop   CAA Q      AAA K      GAA E   UAG Stop   CAG Q      AAG K      GAG E   UGU C      CGU R      AGU S      GGU G   UGC C      CGC R      AGC S      GGC G   UGA Stop   CGA R      AGA R      GGA G   UGG W      CGG R      AGG R      GGG G "
+
+listOfTuples :: [String] -> [(String, String)]
+listOfTuples [] = []
+listOfTuples (k:v:t) = (k, v) : listOfTuples t
+
+splitLen :: Int -> [a] -> [[a]]
+splitLen len str =
+  if length str >= len then
+    [take len str] <> (splitLen len $ (iterate tail str !! len))
+  else
+    []
