b2fd8bd06459317f6a1617bb10b334edf16ff01b
brandly <brandly13@gmail.com>
2018-04-04 13:22:39 -0400

    CONS

---
+++ CONS/main.hs
@@ -0,0 +1,68 @@
+module Main where
+
+import Data.List (intercalate)
+
+input = ">Rosalind_1\nATCCAGCT\n>Rosalind_2\nGGGCAACT\n>Rosalind_3\nATGGATCT\n>Rosalind_4\nAAGCAACC\n>Rosalind_5\nTTGGAACT\n>Rosalind_6\nATGCCATT\n>Rosalind_7\nATGGCACT"
+
+main :: IO ()
+main =
+  putStrLn $ unlines $ [consensus finalCounts, table finalCounts]
+  where
+    finalCounts :: [[(Char, Int)]]
+    finalCounts = foldl update initCounts values
+
+    update :: [[(Char, Int)]] -> String -> [[(Char, Int)]]
+    update counts str =
+      map (uncurry increment) $ zip counts str
+
+    initCounts :: [[(Char, Int)]]
+    initCounts = map (\_ -> []) (values !! 0)
+
+    values :: [String]
+    values = map snd $ parsePairs input
+
+table :: [[(Char, Int)]] -> String
+table counts =
+  unlines
+    $ map (\(c, ints) -> unwords $ (c : ":") : (map show ints))
+    $ map (\c -> (c, forChar c)) "ACGT"
+  where
+    forChar :: Char -> [Int]
+    forChar c =
+      map
+        (\cnt ->
+          case lookup c cnt of
+            Just num -> num
+            Nothing -> 0
+        )
+        counts
+
+consensus :: [[(Char, Int)]] -> String
+consensus counts =
+  map (fst . max) counts
+  where
+    max :: [(Char, Int)] -> (Char, Int)
+    max = foldl (\a b -> if snd b > snd a then b else a) (' ', 0)
+
+increment :: [(Char, Int)] -> Char -> [(Char, Int)]
+increment list c =
+  case lookup c list of
+    Just num ->
+      (c, num + 1) : filter (\(char, _) -> char /= c) list
+    Nothing ->
+      (c, 1) : list
+
+-- wrote this for GC
+parsePairs :: String -> [(String, String)]
+parsePairs str =
+  go $ lines str
+  where
+    go :: [String] -> [(String, String)]
+    go [] = []
+    go (key:lns) =
+      (drop 1 key, intercalate "" $ takeWhile (not . startsWith '>') lns)
+        : (go $ dropWhile (not . startsWith '>') lns)
+
+startsWith :: Char -> String -> Bool
+startsWith c str =
+  str !! 0 == c
